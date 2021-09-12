699b14f5a5c54adf753f4395768d31f377b4963f
Noam <70270328+Noam-Hadar@users.noreply.github.com>
2021-09-12 19:54:51 +0300

    Update BAMdelbee_Batch_Maker.py
    Comments added

--- BAMdelbee_Batch_Maker.py
+++ BAMdelbee_Batch_Maker.py
@@ -1,3 +1,4 @@
+# Made by Noam Hadar
 from glob import glob
 from os import system
 import pandas as pd
@@ -6,23 +7,24 @@ try:
     system("mkdir temp")
 except:
     "temp folder already present"
-bams = glob("*.bam")
+    
+bams = glob("*.bam") #automatically finds all BAM files within local directory to be used as input files.
 print("Located BAM files:\n" +"\n".join(bams) + "\n\nProcessing...")
 
-users = [bam[:-4] for bam in bams]
+users = [bam[:-4] for bam in bams] #remove the ".bam" suffix
 open("temp/samples.txt", 'w').write('\n'.join(users))
 def depthCounter(chromosome):
     try:
         for bam in bams:
-            system("samtools depth -a -r chr" + chromosome + ' ' + bam + " | cut -f2,3 | awk '$1%50==0' | head -n -1 | sort -n -s -k1,1 > temp/" + bam[:-4] + "_chr" + chromosome + ".counted")
+            system("samtools depth -a -r chr" + chromosome + ' ' + bam + " | cut -f2,3 | awk '$1%50==0' | head -n -1 | sort -n -s -k1,1 > temp/" + bam[:-4] + "_chr" + chromosome + ".counted") #counts coverage across each chromosome in steps of 50bps
         df = pd.read_csv("temp/" + bams[0][:-4] + "_chr" + chromosome + ".counted", sep = '\t', header = None)
         df.columns = ['Coordinate', bams[0][:-4]]
         for bam in bams[1:]:
             df2 = pd.read_csv("temp/" + bam[:-4] + "_chr" + chromosome + ".counted", sep = '\t', header = None)
             df2.columns = ['Coordinate', bam[:-4]]
-            df = df.merge(df2, on='Coordinate')
+            df = df.merge(df2, on='Coordinate') #merging all count from each bam file to one table
         df['coverages'] = df.apply(lambda x : x.values[1:], axis = 1) 
-        df = df[df['coverages'].apply(lambda x : (sum(x) != 0) and (0 in x) and (max(x) >= 5))]
+        df = df[df['coverages'].apply(lambda x : (sum(x) != 0) and (0 in x) and (max(x) >= 5))] #ommits all rows with all zeros or less than 5 reads at max for all samples
         del df['coverages']
         df.to_csv("temp/chr" + chromosome + ".coverage", sep = '\t', index = False)
         system("rm -f temp/*hr" + chromosome + ".*ounted")
